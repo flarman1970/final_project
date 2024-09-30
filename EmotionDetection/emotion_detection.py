5221b9bec7d2e64cca9205f22cfef99ccb28eb04
flarman1970 <flarman1970@gmail.com>
2024-09-30 04:43:35 -0400

    prueb

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,43 @@
+import requests
+import json
+def emotion_detector(text_to_analyze):
+    # URL of the sentiment analysis service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    # Constructing the request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    # Custom header specifying the model ID for the sentiment analysis service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    # Sending a POST request to the sentiment analysis API
+    response = requests.post(url, json=myobj, headers=header)
+    
+    # Convertir la respuesta JSON en un diccionario de Python
+    data_text = json.loads(response.text)
+    data = data_text['emotionPredictions'][0]
+
+    # Extraer los valores de las emociones
+    anger_score = data['emotion']['anger']
+    disgust_score = data['emotion']['disgust']
+    fear_score = data['emotion']['fear']
+    joy_score = data['emotion']['joy']
+    sadness_score = data['emotion']['sadness']
+
+    # Determinar la emoción dominante
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+# Crear el diccionario de salida
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    
